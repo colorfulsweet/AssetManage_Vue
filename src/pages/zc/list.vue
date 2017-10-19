5575dc61b15b35adfce6c104f1d3a6c7dc811771
结发受长生 <init_pro@aliyun.com>
2017-10-20 02:56:55 +0800

    清单页面操作

--- src/pages/zc/list.vue
+++ src/pages/zc/list.vue
@@ -1,22 +1,23 @@
+<!-- 流转清单(保存在缓存当中的数据) -->
 <template>
 <div>
 	<x-table :cell-bordered="false" >
     <thead>
       <tr style="background-color: #F7F7F7">
-        <th>序号</th>
         <th>编号</th>
         <th>名称</th>
         <th>数量</th>
         <th>单位</th>
+        <th>移除</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="(item,index) in zcList" :key="item.uuid" v-bind:class="{selected : item.isSelected}">
-        <td>{{index+1}}</td>
         <td>{{item.zcid}}</td>
         <td>{{item.mingch}}</td>
-        <td>{{item.shul}}</td>
+        <td><input class="table_input" :value="item.shul" v-on:change="numChange(index)"/></td>
         <td>{{item.danwei}}</td>
+        <td><span class="vux-close" v-on:click="removeItem(index)"></span></td>
       </tr>
     </tbody>
   </x-table>
@@ -29,6 +30,7 @@
 </template>
 <script>
 import { XTable,XButton } from 'vux'
+
 export default {
   name : "zc_list",
   data () {
@@ -54,10 +56,41 @@ export default {
   components : { XTable,XButton },
   methods : {
     /**
+     * 数量输入框修改事件函数
+     * @param {number} index 数据对应的索引 
+     */
+    numChange (index) {
+      var newVal = parseFloat(event.currentTarget.value);//新值
+      //新值不合法的情况下, 将输入框赋值为上一个有效的值
+      if(Number.isNaN(newVal) || newVal<=0) {
+        this.$vux.toast.text('请输入有效的数字(必须大于0)', 'middle');
+        event.currentTarget.value = this.zcList[index].zcNum || this.zcList[index].shul;
+        return;
+      }
+      if(newVal > this.zcList[index].shul) {//原始值(新值不能超过该值)
+        this.$vux.toast.text(
+            `${this.$store.state.operateList[this.operate-1]}数量不能超过当前库存数量:${this.zcList[index].shul}`,'middle');
+        event.currentTarget.value = this.zcList[index].zcNum || this.zcList[index].shul;
+        return;
+      }
+      event.currentTarget.value = newVal;
+      this.zcList[index].zcNum = newVal;
+    },
+    /**
      * 生成二维码
      */
     toQrcode () {
-
+      if(!this.zcList.length) {
+        this.$vux.toast.text('清单中没有任何数据', 'middle');
+        return;
+      }
+      var zcInfo = {};
+      this.zcList.forEach(function(item){
+        zcInfo[item.uuid] = item.zcNum || item.shul;
+      });
+      this.saveLzxx(function(){
+        //TODO 跳转到二维码展示页面
+      }, zcInfo);
     },
     /**
      * 继续检索添加
@@ -69,10 +102,57 @@ export default {
      * 对方无法扫码
      */
     toReceive () {
-
+      if(!this.zcList.length) {
+        this.$vux.toast.text('清单中没有任何数据', 'middle');
+        return;
+      }
+      var zcInfo = {};
+      this.zcList.forEach(function(item){
+        zcInfo[item.uuid] = item.zcNum || item.shul;
+      });
+      this.saveLzxx(function(){
+        localStorage.setItem("from", "noQrcode");
+        //TODO 跳转到接收页面
+      }, zcInfo);
+    },
+    /**
+     * 移除清单中的项目
+     * @argument index 要移除条目的索引(从0开始)
+     */
+    removeItem (index) {
+      this.zcList.splice(index, 1);
+      var selectIds = this.zcList.map(function(item) {
+        return item.uuid;
+      });
+      localStorage.setItem("selectedIds", JSON.stringify(selectIds));
+    },
+    /**
+     * 保存流转信息
+     * @param {function} callback 保存完成之后的回调函数(可以执行页面跳转) 
+     * @param {object} zcInfo 资产信息 资产ID:数量 键值对结构, 如 {"1001":10,"1002":5.6}
+     */
+    saveLzxx (callback, zcInfo) {
+      this.$http.post(this.$store.state.apiUrl + "lz/save", {
+        bgrId : this.$store.state.loginInfo.userData.uuid,
+        zcInfo : JSON.stringify(zcInfo),
+        operate : localStorage.getItem("operate")
+      }).then(function(response){
+        localStorage.removeItem("selectedIds");
+        localStorage.setItem("operateId", response.data);
+        if(typeof callback === "function") {
+          callback.call(this);
+        }
+      });
     }
   }
-  
 }
 </script>
-
+<style scope lang="less">
+@import '~vux/src/styles/close.less';
+.vux-table .table_input {
+  width: 3.5em;
+  height: 1.4em;
+  line-height: 1.4em;
+  text-align: center;
+}
+</style>
