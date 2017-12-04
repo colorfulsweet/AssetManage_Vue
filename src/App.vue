f1694aadf316f452d679cf0a3c2cb7d4730adb1e
结发受长生 <init_pro@aliyun.com>
2017-12-05 02:15:25 +0800

    修改tab

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <loading ></loading>
-    <x-header :left-options="{showBack: $store.state.headerConf.hasbackbtn}">
-      {{$store.state.headerConf.title}}
-    </x-header>
-    <user-info :realname="$store.state.loginInfo.userData ? $store.state.loginInfo.userData.realname : null"></user-info>
-    <transition :name="transitionName">
-      <router-view class="main-view"/>
-    </transition>
-  </div>
+<div id="app">
+	<loading ></loading>
+	<x-header :left-options="{showBack: $store.state.headerConf.hasbackbtn}">
+		{{$store.state.headerConf.title}}
+	</x-header>
+	<user-info :realname="$store.state.loginInfo.userData ? $store.state.loginInfo.userData.realname : null"></user-info>
+	<transition :name="transitionName">
+		<router-view class="main-view"/>
+	</transition>
+</div>
 </template>
 
 <script>
@@ -17,70 +17,70 @@ import Loading from "./components/loading";
 import UserInfo from "./components/user_info";
 
 export default {
-  name: 'app',
-  data () {
-    return {
-      transitionName: 'slide-left',
-      isLoading : true
-    };
-  },
-  components : {
-    Actionsheet, XHeader, Loading, UserInfo
-  },
-  watch: {
-    '$route' (to, from) {
-      const toDepth = to.path.split('/').length
-      const fromDepth = from.path.split('/').length
-      this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
-    }
-  }
+	name: 'app',
+	data () {
+		return {
+			transitionName: 'slide-left',
+			isLoading : true
+		};
+	},
+	components : {
+		Actionsheet, XHeader, Loading, UserInfo
+	},
+	watch: {
+		'$route' (to, from) {
+			const toDepth = to.path.split('/').length
+			const fromDepth = from.path.split('/').length
+			this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+		}
+	}
 }
 </script>
 <style>
 @import "../static/fonts/font-awesome.min.css";
 * {
-  margin : 0;
-  padding : 0;
+	margin : 0;
+	padding : 0;
 }
 html,body{
-  width: 100%;
-  height:100%;
-  overflow-x:hidden;
-  min-width: 320px;
+	width: 100%;
+	height:100%;
+	overflow-x:hidden;
+	min-width: 320px;
 }
 #app {
-  position: relative;
-  height:100%;
+	position: relative;
+	height:100%;
 }
 .main-view {
-  padding-top:2.9em;
-  position: absolute;
-  width: 100%;
-  height : calc(100vh - 2.9em);
-  transition: all .5s cubic-bezier(.55,0,.1,1);
+	padding-top:2.9em;
+	position: absolute;
+	width: 100%;
+	height : calc(100vh - 2.9em);
+	transition: all .5s cubic-bezier(.55,0,.1,1);
 }
 .slide-left-enter, .slide-right-leave-active {
-  opacity: 0;
-  -webkit-transform: translate(30px, 0);
-  transform: translate(30px, 0);
+	opacity: 0;
+	-webkit-transform: translate(30px, 0);
+	transform: translate(30px, 0);
 }
 .slide-left-leave-active, .slide-right-enter {
-  opacity: 0;
-  -webkit-transform: translate(-30px, 0);
-  transform: translate(-30px, 0);
+	opacity: 0;
+	-webkit-transform: translate(-30px, 0);
+	transform: translate(-30px, 0);
 }
 .vux-header, .weui-tabbar {
-  width:100%;
-  position: fixed !important;
-  z-index:100;
+	width:100%;
+	position: fixed !important;
+	z-index:100;
 }
 .vux-header {
-  top:0;
+	top:0;
 }
 .weui-tabbar {
-  bottom: 0;
+	bottom: 0;
 }
 .btn-container {
-  padding: 1em;
+	padding: 1em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import qs from 'qs'
 import App from './App'
 import router from './router'
 import store from "./store/store"
-import  { ToastPlugin, AjaxPlugin, AlertPlugin } from 'vux'
+import	{ ToastPlugin, AjaxPlugin, AlertPlugin } from 'vux'
 
 Vue.config.productionTip = false
 
@@ -14,45 +14,45 @@ Vue.use(AjaxPlugin); //发送ajax请求插件
 Vue.use(AlertPlugin); //提示框
 
 Vue.http.interceptors.request.use(function (config) {
-  // 请求发送前的处理逻辑
-  vm.$store.commit("setLoading", true);
-  var _token = vm.$store.state.loginInfo.token;
-  //将_token添加到请求参数
-  switch(config.method) {
-    case "get" :
-      if(typeof config.params === "object") {
-        config.params._token = _token;
-      } else {
-        config.params = { _token };
-      }
-      break;
-    case "post" :
-      if(typeof config.data === "object") {
-        config.data._token = _token ? _token : "";
-      } else {
-        config.data = { _token };
-      }
-      //post请求对参数进行序列化
-      config.data = qs.stringify(config.data);
-      break;
-  }
-  return config;
+	// 请求发送前的处理逻辑
+	vm.$store.commit("setLoading", true);
+	var _token = vm.$store.state.loginInfo.token;
+	//将_token添加到请求参数
+	switch(config.method) {
+	case "get" :
+		if(typeof config.params === "object") {
+			config.params._token = _token;
+		} else {
+			config.params = { _token };
+		}
+		break;
+	case "post" :
+		if(typeof config.data === "object") {
+			config.data._token = _token ? _token : "";
+		} else {
+			config.data = { _token };
+		}
+		//post请求对参数进行序列化
+		config.data = qs.stringify(config.data);
+		break;
+	}
+	return config;
 }, function (error) {
-  // Do something with request error 
-  return Promise.reject(error);
+	// Do something with request error 
+	return Promise.reject(error);
 });
 
 Vue.http.interceptors.response.use(function(response){
-  // 请求完成之后的处理逻辑
-  vm.$store.commit("setLoading", false);
-  return response;
+	// 请求完成之后的处理逻辑
+	vm.$store.commit("setLoading", false);
+	return response;
 });
 
 /* eslint-disable no-new */
 const vm = new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App },
-  store
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: { App },
+	store
 })
